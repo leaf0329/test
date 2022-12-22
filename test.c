47586ee917415cd99512713ad9208a1c296f6bd1
Enys10021 <109940826+leaf0329@users.noreply.github.com>
2022-12-22 22:47:53 +0800

    Update test.c

--- test.c
+++ test.c
@@ -4,5 +4,8 @@
 int main()
 {
 	printf("Elysia Is My Wife");
-		return 114514;
+	printf("Success Or Death");
+	printf("No Game No Life");
+	printf("人的一生会经历三次成长，\n第一次，\n是明白事物的对错;\n第二次，\n是明白有些事不止对与错;\n第三次，\n是在明白有些事·没有对与错之后，\n依然坚定地去做自己相信的事，\n并为之负起责任。");
+		return 1145141919810;
 }
